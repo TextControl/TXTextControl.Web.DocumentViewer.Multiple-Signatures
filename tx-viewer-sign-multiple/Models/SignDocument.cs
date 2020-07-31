eeb6b8fe03769d8c5807644e8c38bd39914ffd3e
Bjoern Meyer <bjoern@textcontrol.com>
2020-07-31 08:43:12 +0200

    Added model

---
+++ tx-viewer-sign-multiple/Models/SignDocument.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tx_viewer_sign_multiple.Models
+{
+    public class SignDocument
+    {
+        public string UniqueId { get; set; }
+        public string Document { get; set; }
+
+        public string SignerId { get; set; }
+
+    }
+}
